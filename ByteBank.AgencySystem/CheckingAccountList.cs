2e4632ee0350ddcec0ca4cc7b1a272ae49c64eee
Eduardo da Silva Dörr <edudorr@hotmail.com>
2021-08-31 09:01:34 -0300

    Adicionar arquivos de projeto.

---
+++ ByteBank.AgencySystem/CheckingAccountList.cs
@@ -0,0 +1,19 @@
+﻿using ByteBank.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.AgencySystem {
+  public class CheckingAccountList {
+    private CheckingAccount[] _items;
+
+    public CheckingAccountList() {
+      _items = new CheckingAccount[5];
+    }
+
+    public void Apppend(CheckingAccount account) 
+    }
+  }
+}
---
+++ ByteBank.AgencySystem/Program.cs
@@ -0,0 +1,54 @@
+﻿using ByteBank.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ByteBank.AgencySystem {
+  class Program {
+    static void Main(string[] args) {
+      
+
+      Console.WriteLine("The application finished! Key enter to exit!");
+      Console.ReadLine();
+    }
+
+    static void TestCheckingAccountArray() {
+      CheckingAccount[] account = new CheckingAccount[]{
+        new CheckingAccount(245, 234587),
+        new CheckingAccount(245, 236489),
+        new CheckingAccount(245, 214587)
+    };
+
+      for (int i = 0; i < account.Length; i++) {
+        Console.WriteLine($"Account {i}: Agency {account[i].Agency} / Number {account[i].Number}");
+      }
+
+      for (int i = 0; i < account.Length; i++) {
+        Console.WriteLine($"Account {i}: Agency {account[i].Agency} / Number {account[i].Number}");
+      }
+    }
+
+    static void TestIntArray() {
+      int[] age = new int[5] { 10, 28, 35, 48, 50 };
+      double average = 0;
+
+      //age[0] = 50;
+      //age[1] = 10;
+      //age[2] = 28;
+      //age[3] = 35;
+      //age[4] = 48;
+
+      for (int i = 0; i < age.Length; i++) {
+        average += age[i];
+        Console.WriteLine(i);
+      }
+
+      average /= age.Length;
+
+      Console.WriteLine($"The average age is {average}");
+    }
+  }
+}
